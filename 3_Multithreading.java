954df0eed39a260858ca2d8e60d843da48dea094
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2025-06-10 16:01:00 +0530

    Update 3_Multithreading.java
    added

--- 3_Multithreading.java
+++ 3_Multithreading.java
@@ -1,1 +1,14 @@
-// Test
+// Multithreading in java
+Multithreading is one of the most popular feature of Java programming language as it allows the concurrent execution of two or more parts of a program. 
+Concurrent execution means two or more parts of the program are executing at the same time, this maximizes the CPU utilization and gives you better performance. 
+These parts of the program are called threads.
+
+Threads are independent because they all have separate path of execution that’s the reason if an exception occurs in one thread, it doesn’t affect the execution of other threads. 
+All threads of a process share the common memory. The process of executing multiple threads simultaneously is known as multithreading.
+
+// Advantages of Multithreading
+Efficient CPU Utilization: As more than one threads run independently, this allows the CPU to perform multiple tasks simultaneously.
+Improved Performance
+Better Resource Sharing: As discussed earlier, threads share common memory, this reduces overhead compared to processes.
+
+  
