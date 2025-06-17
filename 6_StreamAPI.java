fac13e06c01c42853c29183e818faef58841458c
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2025-06-18 00:48:17 +0530

    Update 6_StreamAPI.java
    added

--- 6_StreamAPI.java
+++ 6_StreamAPI.java
@@ -1,1 +1,47 @@
-// Stream API
+// Stream API-
+               By using streams we can perform various aggregate operations on the data returned from collections, arrays, Input/Output operations.
+
+A Simple Example of Java Stream Filter()
+                 
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // Using only filter() and forEach to print even numbers
+        numbers.stream()
+               .filter(n -> n % 2 == 0)
+               .forEach(System.out::println);
+    }
+}
+OP-
+2
+4
+6
+Example 1: Stream filter() and collect()
+  
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Melisandre","Sansa","Jon","Daenerys","Joffery");
+
+        List<String> longnames = names.stream()    // converting the list to stream
+                .filter(str -> str.length() > 6)   // filter the stream to create a new stream
+                .collect(Collectors.toList());  // collect the final stream and convert it to a List
+
+        longnames.forEach(System.out::println);           
+    }
+}
+Output:
+
+Melisandre
+Daenerys
+Joffery
+
